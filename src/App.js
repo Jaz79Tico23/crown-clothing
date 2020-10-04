e2e52ee81a5aee638b231f80320970e539a43740
Tim Haas <tmisshawk@aol.com>
2020-10-04 17:18:52 -0500

    deleted unnecessary files. added home page files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Home from "./components/Pages/Home/Home";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
   }
 }
 
