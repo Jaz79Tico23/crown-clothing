8bc4cf498ba8560879e8b6313cbacfb81f016e1d
Tim Haas <tmisshawk@aol.com>
2020-10-06 16:32:05 -0500

    added Contact Page and Shop page files. Created Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+// node_modules
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+// components
+import Header from "./components/Header/Header";
 import Home from "./Pages/Home/Home";
+import Shop from "./Pages/Shop/Shop";
+import Contact from "./Pages/Contact/Contact";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </div>
     );
   }
